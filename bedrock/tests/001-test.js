43645ecee93122210fb64b7dda964989fa598d3b
Dave Longley <dlongley@digitalbazaar.com>
2016-08-03 17:12:34 -0400

    Use `.when` to properly test resolver.

--- bedrock/tests/001-test.js
+++ bedrock/tests/001-test.js
@@ -7,30 +7,48 @@ var bedrock = global.bedrock;
 bedrock.unit('bedrock-angular-resolver', function(bootstrap) {
 
 var $injector;
-var provider;
+var _$routeProvider;
+var _routeResolverProvider;
 var order = [];
 
 describe('routeResolverProvider', function() {
-  beforeEach(function() {
+  before(function() {
     angular.module('test', ['bedrock.resolver'])
-      .config(function(routeResolverProvider) {
-        provider = routeResolverProvider;
+      .config(function($routeProvider, routeResolverProvider) {
+        _$routeProvider = $routeProvider;
+        _routeResolverProvider = routeResolverProvider;
       });
     $injector = bootstrap();
   });
 
+  beforeEach(function(done) {
+    order.splice(0, order.length);
+    // must reset current route brResolve cache because the code runs against
+    // the `current` route and we don't actually change routes during the test
+    // TODO: preferred alternative would be to actually change routes to test
+    // resolution ... or mock changing the route or cancel it before it
+    // finishes or something similar (probably an angular testing pattern
+    // that we can copy)
+    var $route = $injector.get('$route');
+    delete $route.current._brResolving;
+    if('locals' in $route.current) {
+      delete $route.current.brResolve;
+    }
+    done();
+  });
+
   it('should not be undefined', function() {
-    should.exist(provider);
+    should.exist(_routeResolverProvider);
   });
 
   it('should add a resolve function', function() {
-    expect(provider.add('runSecond', 'runFirst', function() {
+    expect(_routeResolverProvider.add('runSecond', 'runFirst', function() {
       order.push(2);
     })).to.not.throw;
   });
 
   it('should add another resolve function', function() {
-    expect(provider.add('runFirst', function($q, $timeout) {
+    expect(_routeResolverProvider.add('runFirst', function($q, $timeout) {
       return $q(function(resolve) {
         $timeout(function() {
           order.push(1);
@@ -40,14 +58,43 @@ describe('routeResolverProvider', function() {
     })).to.not.throw;
   });
 
-  it('should invoke route resolve functions in the proper order', function(done) {
+  it('should invoke global route resolve functions in the proper order', function(done) {
+    // create fake route
+    var routeOptions = {};
+    _$routeProvider.when('/test-route', routeOptions);
+
     var resolves = [];
-    angular.forEach(provider.resolve, function(value, key) {
-      resolves.push($injector.invoke(value, null, null, key));
+    angular.forEach(routeOptions.resolve, function(fn, name) {
+      resolves.push($injector.invoke(fn, null, null, name));
     });
     $injector.get('$q').all(resolves).then(function() {
       order.should.have.members([1, 2]);
       done();
+    }).catch(function(err) {
+      done(err);
+    });
+  });
+
+  it('should invoke global and local route resolve functions in the proper order', function(done) {
+    // create fake route
+    var routeOptions = {
+      resolve: {
+        _test: function() {
+          order.push(3);
+        }
+      }
+    };
+    _$routeProvider.when('/test-route', routeOptions);
+
+    var resolves = [];
+    angular.forEach(routeOptions.resolve, function(fn, name) {
+      resolves.push($injector.invoke(fn, null, null, name));
+    });
+    $injector.get('$q').all(resolves).then(function() {
+      order.should.have.members([1, 2, 3]);
+      done();
+    }).catch(function(err) {
+      done(err);
     });
   });
 });
